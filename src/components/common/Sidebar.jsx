ac58447e9c1becbba797127da0c9287bb761beb4
Priyan Darshana <priyan.abewickrama@ideahub.lk>
2024-01-26 13:58:55 +0530

    main body sctructure

---
+++ src/components/common/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Sidebar = () => {
+    return (
+        <>
+            <div className="pd-sidebar">
+                <div className="pd-sidebar__header">
+                    <h2>Scandinavia</h2>
+                </div>
+            </div>
+
+        </>
+    )
+}
--- src/components/pages/HomeWrapper.jsx
+++ src/components/pages/HomeWrapper.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Sidebar } from "../common/Sidebar";
 
 export const HomeWrapper = () => {
     return (
         <>
             <div className="pd-h-w">
-                <h2>Home</h2>
+                <Container>
+                    <Row>
+                        <Col lg="4">
+                            <Sidebar />
+                        </Col>
+                        <Col lg="8">
+
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </>
     )

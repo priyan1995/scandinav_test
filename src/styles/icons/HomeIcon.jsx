57b6977dfa0a0bfcbc78b7a71f5d99b7ee55bb8d
Priyan Darshana <priyan.abewickrama@ideahub.lk>
2024-01-27 17:48:55 +0530

    remove console error

--- src/styles/icons/HomeIcon.jsx
+++ src/styles/icons/HomeIcon.jsx
@@ -7,7 +7,7 @@ export const HomeIcon = (props) => {
     return (
         <>
             <div className="pd-home-icon">
-                <svg width="800px" height="800px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><defs></defs><polygon class="a" points="43.5 21.446 24 6.554 4.5 21.446 4.5 41.446 43.5 41.446 43.5 21.446" /></svg>
+                <svg width="800px" height="800px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><defs></defs><polygon className="a" points="43.5 21.446 24 6.554 4.5 21.446 4.5 41.446 43.5 41.446 43.5 21.446" /></svg>
             </div>
 
         </>
